e75d8027b9eca03e062d5bcd3e59fe7cb2697571
svplaksin <sv-plaksin@yandex.ru>
2023-11-28 15:45:49 +0300

    Refactor import statement to 'from config import settings'. It allow to make a successful alembic migration to add new tables in database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-from config import settings
+from app.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL)
 
