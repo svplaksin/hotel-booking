be5609eab621d3edf547b153e84d26a813c981a8
svplaksin <sv-plaksin@yandex.ru>
2023-11-28 14:23:41 +0300

    Create app/config.py and move private variables into it from database.py

---
+++ app/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self):
+        user = f"{self.DB_USER}:{self.DB_PASS}"
+        database = f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{user}@{database}"
+
+    # Со 2 версии Pydantic класс Config был заменен на атрибут model_config
+    # class Config:
+    #     env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+
+print(settings.DATABASE_URL)
--- app/database.py
+++ app/database.py
@@ -1,15 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_USER = "postgres"
-DB_PASS = "postgres"
-DB_NAME = "postgres"
+from config import settings
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
