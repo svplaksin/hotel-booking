c5ee5123ac308f03b9c3776b5e0f58601cce6835
svplaksin <sv-plaksin@yandex.ru>
2023-12-07 11:30:46 +0300

    Add "login" endpoint in app/users/router.py

--- app/users/router.py
+++ app/users/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from app.users.auth import get_password_hash
+from fastapi import APIRouter, HTTPException, Response, status
+from app.users.auth import authentificate_user, create_access_token, get_password_hash
 from app.users.dao import UsersDAO
 
-from app.users.schemas import SUserRegister
+from app.users.schemas import SUserAuth
 
 
 router = APIRouter(
@@ -12,9 +12,19 @@ router = APIRouter(
 
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authentificate_user(user_data.email, user_data.password)
+    if not user:
+        return HTTPException(status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return {"access_token": access_token}
