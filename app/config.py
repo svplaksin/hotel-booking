1fd91a079b05d652f04e727ca16f6a7bf47c8a2b
svplaksin <sv-plaksin@yandex.ru>
2023-12-07 12:33:27 +0300

    Add jwt_encode_key and jwt_encode_algorithm into settings.py and .env

--- app/config.py
+++ app/config.py
@@ -7,12 +7,22 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    ENCODE_KEY: str
+    ENCODE_ALGORITHM: str
 
     @property
     def DATABASE_URL(self):
         user = f"{self.DB_USER}:{self.DB_PASS}"
         database = f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         return f"postgresql+asyncpg://{user}@{database}"
+
+    @property
+    def ENCODE_KEY(self):
+        return f"{self.ENCODE_KEY}"
+
+    @property
+    def ENCODE_ALGORITHM(self):
+        return f"{self.ENCODE_ALGORITHM}"
 
     # Со 2 версии Pydantic класс Config был заменен на атрибут model_config
     # class Config:
