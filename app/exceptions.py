1fc9fc9f8ad0680ba320a725d7080db1164176d0
svplaksin <sv-plaksin@yandex.ru>
2023-12-11 10:22:31 +0300

    Fix exceptions.py

--- app/exceptions.py
+++ app/exceptions.py
@@ -3,37 +3,61 @@ from fastapi import HTTPException, status
 
 
 class BookingException(HTTPException):
-    status_code = 500
-    detail = ""
-
-    def __init__(self, status_code: int, detail: Any = None) -> None:
+    def __init__(
+        self, status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail: Any = ""
+    ) -> None:
         super().__init__(status_code, detail)
 
 
 class UserAlreadyExistsException(HTTPException):
-    status_code = status.HTTP_409_CONFLICT,
-    detail = "Пользователь с таким email уже существует"
+    def __init__(
+            self, status_code: int = status.HTTP_409_CONFLICT,
+            detail: Any = "Пользователь с таким email уже существует") -> None:
+        super().__init__(status_code, detail)
 
 
 class IncorrectEmailOrPasswordException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "Неверно указана почта или пароль"
+    def __init__(
+        self, status_code: int = status.HTTP_401_UNAUTHORIZED,
+        detail: Any = "Неверно указана почта или пароль"
+    ) -> None:
+        super().__init__(status_code, detail)
 
 
 class TokenExpiredException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "Срок действия токена истек"
+    def __init__(
+        self, status_code: int = status.HTTP_401_UNAUTHORIZED,
+        detail: Any = "Срок действия токена истек"
+    ) -> None:
+        super().__init__(status_code, detail)
 
 
 class TokenAbsentException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "Токен отсутствует"
+    def __init__(
+        self, status_code: int = status.HTTP_401_UNAUTHORIZED,
+        detail: Any = "Токен отсутствует"
+    ) -> None:
+        super().__init__(status_code, detail)
 
 
 class IncorrectTokenFormatException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "Неверный формат токена"
+    def __init__(
+        self, status_code: int = status.HTTP_401_UNAUTHORIZED,
+        detail: Any = "Неверный формат токена"
+    ) -> None:
+        super().__init__(status_code, detail)
 
 
 class UserMissingException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
+    def __init__(
+        self, status_code: int = status.HTTP_401_UNAUTHORIZED
+    ) -> None:
+        super().__init__(status_code)
+
+
+class RoomCannotBeBooked(HTTPException):
+    def __init__(
+            self, status_code: int = status.HTTP_409_CONFLICT,
+            detail: Any = "Не осталось свободных номеров") -> None:
+        super().__init__(status_code, detail)
