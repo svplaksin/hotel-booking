a3bd02cc8f7dbc12d04679ae1ab1939b2ab42895
svplaksin <sv-plaksin@yandex.ru>
2023-12-15 15:54:22 +0300

    Add frontend stuff

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from fastapi import Depends, FastAPI, Query
+from fastapi.staticfiles import StaticFiles
+
 from typing import Optional
 from datetime import date
 
@@ -7,14 +9,21 @@ from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+
+app.include_router(router_pages)
+app.include_router(router_images)
 
 
 @dataclass
