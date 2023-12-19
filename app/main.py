8e37a63b4bdb3ed6e9f2d95b43b9e678dbb09497
svplaksin <sv-plaksin@yandex.ru>
2023-12-19 10:46:24 +0300

    Delete unnecessary endpoints in main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass
-from fastapi import Depends, FastAPI, Query
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from typing import Optional
-from datetime import date
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
@@ -24,19 +21,3 @@ app.include_router(router_rooms)
 
 app.include_router(router_pages)
 app.include_router(router_images)
-
-
-@dataclass
-class HotelSearchArgs():
-    location: str
-    date_from: date
-    date_to: date
-    has_spa: Optional[bool] = None
-    stars: int = Query(None, ge=1, le=5)
-
-
-@app.get("/hotels")
-def get_hotels(
-    search_args: HotelSearchArgs = Depends()
-):
-    return search_args
