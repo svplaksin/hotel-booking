54ca730b42b2a78fb8925c8e9672812e5d668fa9
svplaksin <sv-plaksin@yandex.ru>
2023-12-27 10:55:09 +0300

    Import Redis credentials in main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from redis import asyncio as aioredis
 
 
 from app.bookings.router import router as router_bookings
+from app.config import settings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
@@ -19,7 +20,7 @@ from app.images.router import router as router_images
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    redis = aioredis.from_url("redis://localhost:6379", encoding="utf8")
+    redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", encoding="utf8")
     FastAPICache.init(RedisBackend(redis), prefix="cache")
     yield
 
