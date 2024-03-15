829b3197aa5c06c445ac5ed12e0fd31dc6aa8850
svplaksin <sv-plaksin@yandex.ru>
2024-03-15 12:52:45 +0300

    Add celery beat tasks to remind of upcoming bookings

--- app/tasks/celery_app.py
+++ app/tasks/celery_app.py
@@ -1,10 +1,24 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from app.config import settings
-
 
 celery = Celery(
     "tasks",
     broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
-    include=["app.tasks.tasks"]
+    include=[
+        "app.tasks.tasks",
+        "app.tasks.scheduled"
+    ]
 )
+
+celery.conf.beat_schedule = {
+    "remind_1day": {
+        "task": "booking_reminder_1day",
+        "schedule": crontab(minute="0", hour="9"),
+    },
+    "remind_3days": {
+        "task": "booking_reminder_3days",
+        "schedule": crontab(minute="30", hour="15")
+    }
+}
--- app/tasks/email_templates.py
+++ app/tasks/email_templates.py
@@ -3,6 +3,7 @@ from email.message import EmailMessage
 from pydantic import EmailStr
 
 from app.config import settings
+from app.tasks.utils import plural_days
 
 
 def create_booking_confirmation_template(
@@ -18,6 +19,28 @@ def create_booking_confirmation_template(
     email.set_content(
         f"""
         <h1>Подтвердите бронирование</h1>
+        Вы забронировали отель с {booking["date_from"]} по {booking["date_to"]}
+        """,
+        subtype="html"
+    )
+    return email
+
+
+def create_booking_reminder_template(
+    booking: dict,
+    email_to: EmailStr,
+    days: int
+):
+    email = EmailMessage()
+
+    days_str = plural_days(days)
+    email["Subject"] = f"Осталось {days_str} до заселения"
+    email["From"] = settings.SMTP_USER
+    email["To"] = email_to
+
+    email.set_content(
+        f"""
+        <h1>Напоминание о бронировании</h1>
         Вы забронировали отель с {booking["date_from"]} по {booking["date_to"]}
         """,
         subtype="html"
